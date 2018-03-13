33b06883bc58dd7effeffbc419a1cad2b872f335
vzts <yeechan.ko@gmail.com>
2018-03-14 01:23:55 +0800

    added schedule lib, collector & scheduler done

--- collector/collector.py
+++ collector/collector.py
@@ -1,6 +1,25 @@
 from api.coinone_api import CoinoneApi, CoinoneCurrency
 from api.korbit_api import KorbitApi, KorbitCurrency
 from pymongo import MongoClient
+import schedule
+import functools
+import time
+
+
+def catch_exceptions(cancel_on_failure=False):
+    def catch_exceptions_decorator(job_func):
+        @functools.wraps(job_func)
+        def wrapper(*args, **kwargs):
+            try:
+                return job_func(*args, **kwargs)
+            except:
+                import traceback
+                print("Error at " + time.ctime())
+                print(traceback.format_exc())
+                if cancel_on_failure:
+                    return schedule.CancelJob
+        return wrapper
+    return catch_exceptions_decorator
 
 
 class Collector:
@@ -30,10 +49,12 @@ class Collector:
         self.kb_ma_mb_col = self.kb_db[currency + "_ma_mb"]
         self.kb_filled_orders_col = self.kb_db[currency + "_filled_orders"]
 
+    @catch_exceptions()
     def collect_co_ticker(self):
         co_ticker = self.co_api.get_ticker(self.co_currency)
         self.co_ticker_col.insert_one(co_ticker)
 
+    @catch_exceptions()
     def collect_co_orderbook(self):
         co_orderbook = self.co_api.get_orderbook(self.co_currency)
         self.co_orderbook_col.insert_one(co_orderbook)
@@ -44,14 +65,17 @@ class Collector:
         }
         self.co_ma_mb_col.insert_one(co_ma_mb)
 
+    @catch_exceptions()
     def collect_co_filled_orders(self):
-        co_filled_orders = self.co_api.get_filled_orders(self.co_currency, "day")
+        co_filled_orders = self.co_api.get_filled_orders(self.co_currency, "hour")
         self.co_filled_orders_col.insert_many(co_filled_orders)
 
+    @catch_exceptions()
     def collect_kb_ticker(self):
         kb_ticker = self.kb_api.get_ticker(self.kb_currency)
         self.kb_ticker_col.insert_one(kb_ticker)
 
+    @catch_exceptions()
     def collect_kb_orderbook(self):
         kb_orderbook = self.kb_api.get_orderbook(self.kb_currency)
         self.kb_orderbook_col.insert_one(kb_orderbook)
@@ -62,10 +86,7 @@ class Collector:
         }
         self.kb_ma_mb_col.insert_one(kb_ma_mb)
 
+    @catch_exceptions()
     def collect_kb_filled_orders(self):
-        kb_filled_orders = self.kb_api.get_filled_orders(self.kb_currency, "day")
+        kb_filled_orders = self.kb_api.get_filled_orders(self.kb_currency, "hour")
         self.kb_filled_orders_col.insert_many(kb_filled_orders)
-
-
-collector = Collector("eth")
-collector.collect_co_filled_orders()
---
+++ collector/scheduler.py
@@ -0,0 +1,32 @@
+from collector import Collector
+import threading
+import schedule
+import time
+
+
+def run_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
+
+
+collector = Collector("eth")
+
+# coinone
+schedule.every(3).seconds.do(run_threaded, collector.collect_co_ticker)
+schedule.every(3).seconds.do(run_threaded, collector.collect_co_orderbook)
+schedule.every().hour.do(run_threaded, collector.collect_co_filled_orders)
+
+# korbit
+schedule.every(3).seconds.do(run_threaded, collector.collect_kb_ticker)
+schedule.every(3).seconds.do(run_threaded, collector.collect_kb_orderbook)
+schedule.every().hour.do(run_threaded, collector.collect_kb_filled_orders)
+
+# run initial
+schedule.run_all()
+
+while True:
+    try:
+        schedule.run_pending()
+    except KeyboardInterrupt:
+        print("Collector Bot stopped at " + time.ctime())
+        exit(0)
