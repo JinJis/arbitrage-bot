09945e6b757f6c1b6b4719f7e51924fe4f549049
vzts <yeechan.ko@gmail.com>
2018-03-13 21:43:45 +0800

    working on collector

---
+++ collector/collector.py
@@ -0,0 +1,42 @@
+from api.coinone_api import CoinoneApi, CoinoneCurrency
+from api.korbit_api import KorbitApi, KorbitCurrency
+from pymongo import MongoClient
+
+
+class Collector:
+
+    def __init__(self, currency: str):
+        # make sure currency is in lower-case format
+        if not currency.islower():
+            raise Exception("<currency> parameter should be a lower-cased symbol of the target currency!")
+
+        # init mongo client
+        self.client = MongoClient("127.0.0.1", 27017)
+
+        # init coinone related
+        self.co_api = CoinoneApi()
+        self.co_currency = CoinoneCurrency[currency.upper()].val
+        self.co_db = self.client["coinone"]
+        self.co_ticker_col = self.co_db[currency + "_ticker"]
+        self.co_orderbook_col = self.co_db[currency + "_orderbook"]
+        self.co_ma_mb_col = self.co_db[currency+"_ma_mb"]
+        self.co_filled_order_col = self.co_db[currency + "_filled_order"]
+
+        # init korbit related
+        self.kb_api = KorbitApi()
+        self.kb_db = self.client["korbit"]
+        self.kb_currency = KorbitCurrency[currency.upper()].val
+        self.kb_ticker_col = self.kb_db[currency + "_ticker"]
+        self.kb_orderbook_col = self.kb_db[currency + "_orderbook"]
+        self.kb_ma_mb_col = self.kb_db[currency+"_ma_mb"]
+        self.kb_filled_order_col = self.kb_db[currency + "_filled_order"]
+
+    def collect_co_ticker(self):
+        co_ticker = self.co_api.get_ticker(self.co_currency)
+        self.co_ticker_col.insert_one(co_ticker)
+
+    def collect_kb_ticker(self):
+        kb_ticker = self.kb_api.get_ticker(self.kb_currency)
+        self.kb_ticker_col.insert_one(kb_ticker)
+
+    
