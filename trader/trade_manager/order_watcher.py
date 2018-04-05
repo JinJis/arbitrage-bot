b04d5700057a5d7b73139b935b43f0237504f48e
vzts <yeechan.ko@gmail.com>
2018-04-05 18:24:41 +0800

    order_watcher_stats integration in order_watcher

--- trader/trade_manager/order_watcher.py
+++ trader/trade_manager/order_watcher.py
@@ -7,6 +7,7 @@ from api.coinone_api import CoinoneApi
 from api.korbit_api import KorbitApi
 from config.global_conf import Global
 from config.shared_mongo_client import SharedMongoClient
+from .order_watcher_stats import OrderWatcherStats
 
 
 class OrderWatcher(Thread):
@@ -19,12 +20,12 @@ class OrderWatcher(Thread):
     def is_watchable(order: Order):
         return order.market in OrderWatcher.supported_markets.keys()
 
-    def __init__(self, order: Order, interval_sec: int, risk_flag_sec: int):
+    def __init__(self, order: Order, interval_sec: int, delayed_flag_sec: int):
         super().__init__()
         self.order = order
         self.interval_sec = interval_sec
-        self.risk_flag_sec = risk_flag_sec
-        self.is_at_risk = False
+        self.delayed_flag_sec = delayed_flag_sec
+        self.is_delayed = False
         self.order_col = SharedMongoClient.get_pdb_order_col()
 
         # find matching api
@@ -51,28 +52,40 @@ class OrderWatcher(Thread):
         if not self.is_watchable(self.order):
             return
 
+        # add in order watcher stats
+        OrderWatcherStats.started(self.order.order_id)
+
         # log initial time
         initial_time = time.time()
 
-        while self.order.status is not OrderStatus.FILLED:
-            start_time = time.time()
-            self.do_interval()
-            end_time = time.time()
+        try:
+            while self.order.status is not OrderStatus.FILLED:
+                start_time = time.time()
+                self.do_interval()
+                end_time = time.time()
 
-            # if current time has exceeded the max_wait_sec
-            if not self.is_at_risk:
-                time_check = end_time - initial_time
-                if time_check > self.risk_flag_sec:
-                    self.is_at_risk = True
-                    message = "Order %s has exceeded risk flag time! " \
-                              "Counter measures are expected to be executed manually!" % self.order.order_id
-                    logging.critical(message)
-                    Global.send_to_slack_channel(message)
+                # if current time has exceeded the max_wait_sec
+                if not self.is_delayed:
+                    time_check = end_time - initial_time
+                    if time_check > self.delayed_flag_sec:
+                        self.is_delayed = True
+                        OrderWatcherStats.delayed(self.order.order_id)
+                        message = "Order %s has exceeded delayed flag time! " \
+                                  "Counter measures are expected to be executed manually!" % self.order.order_id
+                        logging.critical(message)
+                        Global.send_to_slack_channel(message)
 
-            # wait for the target interval
-            wait_time = self.interval_sec - (end_time - start_time)
-            if wait_time > 0:
-                time.sleep(wait_time)
+                # wait for the target interval
+                wait_time = self.interval_sec - (end_time - start_time)
+                if wait_time > 0:
+                    time.sleep(wait_time)
 
-        if self.order.status is OrderStatus.FILLED:
-            logging.info(self.order.get_filled_status())
+            # if it is filled
+            if self.order.status is OrderStatus.FILLED:
+                OrderWatcherStats.done(self.order.order_id)
+                logging.info(self.order.get_filled_status())
+
+        except Exception as e:
+            # if there was any error for some unexpected reasons
+            OrderWatcherStats.error(self.order.order_id)
+            logging.error(e)
