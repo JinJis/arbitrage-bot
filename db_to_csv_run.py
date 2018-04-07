924f5367f043c5879b4334ff41571cecaadeed44
dingzhen93 <chungjin93@gmail.com>
2018-04-08 03:37:18 +0900

    db_to_csv pre-setting
    jjang jam

--- db_to_csv_run.py
+++ db_to_csv_run.py
@@ -4,4 +4,15 @@ from collector.db_to_csv import DbToCsv
 db_to_csv = DbToCsv(False)
 start_time = Global.convert_local_datetime_to_epoch("2018.04.06 10:00:00", timezone="kr")
 end_time = Global.convert_local_datetime_to_epoch("2018.04.06 22:00:00", timezone="kr")
-db_to_csv.save_processed_info("coinone", "eth", start_time, end_time)
+
+"""Get Orderbook"""
+#db_to_csv.save_processed_info("coinone", "eth", start_time, end_time)
+#db_to_csv.save_processed_info("korbit", "eth", start_time, end_time)
+
+"""Get Ticker"""
+#db_to_csv.save_ticker_as_csv("coinone", "eth", start_time, end_time)
+#db_to_csv.save_ticker_as_csv("korbit", "eth", start_time, end_time)
+
+"""Get VWAP mid price"""
+db_to_csv.save_mid_vwap_mid_price("coinone", "eth", start_time, end_time, 5)
+db_to_csv.save_mid_vwap_mid_price("korbit", "eth", start_time, end_time, 5)
