011cd29c4bce38e46e3ef633f8848df5bf6a4a37
vzts <yeechan.ko@gmail.com>
2018-04-10 19:58:39 +0800

    lots of refactoring on collector & scheduler...
    him dul da.....

--- collector/scheduler/filled_order_scheduler.py
+++ collector/scheduler/filled_order_scheduler.py
@@ -1,35 +1,13 @@
-from collector.filled_order_collector import FilledOrderCollector
-from collector.scheduler.base_scheduler import BaseScheduler
+from collector.scheduler.api_scheduler import ApiScheduler
+from collector.scheduler.api_scheduler import BaseScheduler
 from config.global_conf import Global
-from pymongo import MongoClient
-from api.coinone_api import CoinoneApi, CoinoneCurrency
-from api.korbit_api import KorbitApi, KorbitCurrency
 
 
-class FilledOrderScheduler(BaseScheduler):
-    def __init__(self, currency: str, should_use_localhost_db: bool = True):
-        # make sure currency is in lower-case format
-        if not currency.islower():
-            raise Exception("<currency> parameter should be a lower-cased symbol of the target currency!")
-
-        super().__init__()
-        mongodb_uri = Global.read_mongodb_uri(should_use_localhost_db)
-        db_client = MongoClient(mongodb_uri)
-        coinone_db = db_client["coinone"]
-        korbit_db = db_client["korbit"]
-        coinone_api = CoinoneApi.instance(True)
-        korbit_api = KorbitApi.instance(True)
-        coinone_currency = CoinoneCurrency[currency.upper()]
-        korbit_currency = KorbitCurrency[currency.upper()]
-        filled_orders_col_name = currency + "_filled_orders"
-
-        self.kb_collector = FilledOrderCollector(korbit_api, korbit_currency, korbit_db[filled_orders_col_name])
-        self.co_collector = FilledOrderCollector(coinone_api, coinone_currency, coinone_db[filled_orders_col_name])
-
+class FilledOrderScheduler(ApiScheduler):
     @BaseScheduler.interval_waiter(3)
     def _actual_run_in_loop(self):
-        Global.run_threaded(self.kb_collector.collect_filled_orders)
         Global.run_threaded(self.co_collector.collect_filled_orders)
+        Global.run_threaded(self.kb_collector.collect_filled_orders)
 
 
 if __name__ == "__main__":
--- collector/scheduler/ticker_orderbook_scheduler.py
+++ collector/scheduler/ticker_orderbook_scheduler.py
@@ -1,23 +1,18 @@
 import time
-from collector.collector import Collector
-from collector.scheduler.base_scheduler import BaseScheduler
 from config.global_conf import Global
+from collector.scheduler.api_scheduler import ApiScheduler
+from collector.scheduler.base_scheduler import BaseScheduler
 
 
-class TickerOrderbookScheduler(BaseScheduler):
-    def __init__(self, should_use_localhost_db: bool = True):
-        super().__init__()
-        mongodb_uri = Global.read_mongodb_uri(should_use_localhost_db)
-        self.collector = Collector(mongodb_uri, "eth")
-
+class TickerOrderbookScheduler(ApiScheduler):
     @BaseScheduler.interval_waiter(5)
     def _actual_run_in_loop(self):
         request_time = int(time.time())
-        Global.run_threaded(self.collector.collect_co_ticker, [request_time])
-        Global.run_threaded(self.collector.collect_co_orderbook, [request_time])
-        Global.run_threaded(self.collector.collect_kb_ticker, [request_time])
-        Global.run_threaded(self.collector.collect_kb_orderbook, [request_time])
+        Global.run_threaded(self.co_collector.collect_ticker, [request_time])
+        Global.run_threaded(self.co_collector.collect_orderbook, [request_time])
+        Global.run_threaded(self.kb_collector.collect_ticker, [request_time])
+        Global.run_threaded(self.kb_collector.collect_orderbook, [request_time])
 
 
 if __name__ == "__main__":
-    TickerOrderbookScheduler().run()
+    TickerOrderbookScheduler("eth").run()
