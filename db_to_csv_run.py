0f2694c2c2fd5dd522bfe7296f24256dda13188e
dingzhen93 <chungjin93@gmail.com>
2018-04-11 23:18:25 +0900

    minor fixed

--- db_to_csv_run.py
+++ db_to_csv_run.py
@@ -22,5 +22,5 @@ end_time = Global.convert_local_datetime_to_epoch("2018.04.07 22:00:00", timezon
 # db_to_csv.save_filled_orders_as_csv("korbit", "eth", start_time * 1000, end_time * 1000)
 
 """Get Orderbook with indexed price & amount"""
-db_to_csv.save_order_book_index("coinone", "eth", start_time, end_time, 5)
-db_to_csv.save_order_book_index("korbit", "eth", start_time * 1000, end_time * 1000, 5)
+db_to_csv.save_orderbook_index("coinone", "eth", start_time, end_time, 5)
+# db_to_csv.save_orderbook_index("korbit", "eth", start_time * 1000, end_time * 1000, 5)
