971186f2d15a801eef07cd47b1911695883c8289
dingzhen93 <chungjin93@gmail.com>
2018-05-01 01:29:23 +0900

    constant occurence of void gopax orderbook asks&bids fixer added

--- run_db_fixer.py
+++ run_db_fixer.py
@@ -4,5 +4,9 @@ from config.global_conf import Global
 
 Global.configure_default_root_logging()
 SharedMongoClient.initialize(should_use_localhost_db=False)
-DbFixer.add_missing_item_with_plain_copy_prev("coinone", "eth_orderbook", "korbit", "eth_orderbook",
-                                              1523454835, 1523455200)
+# DbFixer.add_missing_item_with_plain_copy_prev("coinone", "eth_orderbook", "korbit", "eth_orderbook",
+#                                               1523454835, 1523455200)
+
+start_time = Global.convert_local_datetime_to_epoch("2018.04.30 04:00:00", timezone="kr")
+end_time = Global.convert_local_datetime_to_epoch("2018.04.30 17:00:00", timezone="kr")
+DbFixer.fill_empty_orderbook_entry("gopax", "btc_orderbook", start_time, end_time)
