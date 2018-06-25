50ef26a54cfdc566ab980493a4fe110a9d1cb8c5
dingzhen93 <chungjin93@gmail.com>
2018-06-25 13:28:12 +0900

    Backtester Refactoring completed

--- backtester/risk_free_arb_backtest.py
+++ backtester/risk_free_arb_backtest.py
@@ -1,65 +1,55 @@
 import logging
-from trader.market.market import Market
+from pymongo.cursor import Cursor
 from analyzer.analyzer import BasicAnalyzer
 from analyzer.analyzer import ATSAnalyzer, SpreadInfo
-from config.shared_mongo_client import SharedMongoClient
-from trader.base_arb_bot import BaseArbBot
 from trader.market.trade import Trade, TradeTag, TradeMeta
-from trader.market_manager.virtual_market_manager import VirtualMarketManager
 from trader.trade_manager.trade_manager import TradeManager
 from trader.market_manager.global_fee_accumulator import GlobalFeeAccumulator
+from trader.market_manager.virtual_market_manager import VirtualMarketManager
 
 
 class RfabBacktester:
     TARGET_STRATEGY = ATSAnalyzer.actual_tradable_spread_strategy
 
-    def __init__(self, mm1_balance_dict: dict, mm2_balance_dict: dict, initial_setting_dict: dict,
-                 start_time: int, end_time: int, target_currency: str = None, is_init_setting_opt: bool = False):
-
-        self.mm1 = VirtualMarketManager(mm1_balance_dict["mkt_tag"], mm1_balance_dict["market_fee"],
-                                        mm1_balance_dict["krw_balance"],
-                                        mm1_balance_dict["coin_balance"], target_currency)
-        self.mm2 = VirtualMarketManager(mm2_balance_dict["mkt_tag"], mm2_balance_dict["market_fee"],
-                                        mm2_balance_dict["krw_balance"],
-                                        mm2_balance_dict["coin_balance"], target_currency)
-
-        self.mm1_data_col = self.initialize_mongo(mm1_balance_dict, target_currency)
-        self.mm2_data_col = self.initialize_mongo(mm2_balance_dict, target_currency)
+    def __init__(self, mm1: VirtualMarketManager, mm2: VirtualMarketManager, target_currency: str,
+                 initial_setting_dict: dict, is_init_setting_opt: bool = False):
+        self.mm1 = mm1
+        self.mm2 = mm2
         self.target_currency = target_currency
         self.init_setting_dict = initial_setting_dict
-        self.start_time = start_time
-        self.end_time = end_time
-        self.new_oppty_counter = 0
-        self.rev_oppty_counter = 0
-
-        self.trade_manager = TradeManager(should_db_logging=True, is_backtesting=True)
-        self.mm1_currency = self.mm1.get_market_currency(self.target_currency)
-        self.mm2_currency = self.mm2.get_market_currency(self.target_currency)
-        self.cur_trade = None
         self.is_init_setting_opt = is_init_setting_opt
 
-    def run(self):
-        # get cursor from mongodb
-        mm1_data_cursor, mm2_data_cursor = \
-            BaseArbBot.get_data_from_db(self.mm1_data_col, self.mm2_data_col,
-                                        self.start_time, self.end_time)
+        self.mm1_currency = self.mm1.get_market_currency(self.target_currency)
+        self.mm2_currency = self.mm2.get_market_currency(self.target_currency)
+
+        self.trade_manager = TradeManager(should_db_logging=True, is_backtesting=True)
+        self.trade_logger = TradeInfoLogger(self.trade_manager, self.target_currency)
+
+    def run(self, mm1_data_cursor: Cursor, mm2_data_cursor: Cursor):
         # loop through history data
         for mm1_data, mm2_data in zip(mm1_data_cursor, mm2_data_cursor):
             self.actual_trade_loop(mm1_data, mm2_data)
 
         # log backtesting result
         if not self.is_init_setting_opt:
-            trade_logger = TradeInfoLogger(self.trade_manager, self.target_currency, self.new_oppty_counter,
-                                           self.rev_oppty_counter)
-            trade_logger.log_trade_info()
-            trade_logger.log_oppty_info()
-            trade_logger.log_combined_balance(self.mm1.balance, self.mm2.balance)
+            self.trade_logger.log_trade_info()
+            self.trade_logger.log_oppty_info()
+            self.trade_logger.log_combined_balance(self.mm1.balance, self.mm2.balance)
 
-    def actual_trade_loop(self, mm1_data, mm2_data):
+    def add_trade(self, trade: Trade, spread_info: SpreadInfo):
+        if not trade:
+            return
+        self.trade_manager.add_trade(trade)
+        if not self.is_init_setting_opt:
+            TradeInfoLogger.execute_trade_log_info(trade, spread_info)
+
+    def actual_trade_loop(self, mm1_data: dict, mm2_data: dict):
         # adjust orderbook for realistic backtesting
         self.mm1.apply_history_to_orderbook(mm1_data)
         self.mm2.apply_history_to_orderbook(mm2_data)
 
+        new_trade = None
+        rev_trade = None
         spread_info = RfabBacktester.TARGET_STRATEGY(mm1_data, mm2_data, self.mm1.market_fee, self.mm2.market_fee,
                                                      self.init_setting_dict["max_trading_coin"])
 
@@ -67,83 +57,70 @@ class RfabBacktester:
         new_spread_info = spread_info["new"]
         rev_spread_info = spread_info["rev"]
 
-        # make decision
         # NEW
         if new_spread_info.spread_in_unit > 0:
-            # increase oppty count
-            self.new_oppty_counter += 1
-
-            # decide whether to trade
-            buy_order, sell_order = self.trade_logic(new_spread_info, self.mm1, self.mm2, self.target_currency,
-                                                     self.mm1_currency, self.mm2_currency, self.init_setting_dict,
-                                                     trade_tag="new")
-            # if there was any execution, add trade
-            if buy_order and sell_order:
-                self.cur_trade = Trade(TradeTag.NEW, [buy_order, sell_order], TradeMeta({}))
-                self.trade_manager.add_trade(self.cur_trade)
+            self.trade_logger.acc_oppty_counter("new")
+            new_trade = self.execute_trade(new_spread_info, "new")
+            self.add_trade(new_trade, new_spread_info)
 
         # REVERSE
         if rev_spread_info.spread_in_unit > 0:
-            self.rev_oppty_counter += 1
-            # decide whether to trade
-            buy_order, sell_order = self.trade_logic(rev_spread_info, self.mm2, self.mm1, self.target_currency,
-                                                     self.mm2_currency, self.mm1_currency, self.init_setting_dict,
-                                                     trade_tag="rev")
-            # if there was any execution, add trade
-            if buy_order and sell_order:
-                self.cur_trade = Trade(TradeTag.REV, [buy_order, sell_order], TradeMeta({}))
-                self.trade_manager.add_trade(self.cur_trade)
+            self.trade_logger.acc_oppty_counter("rev")
+            rev_trade = self.execute_trade(new_spread_info, "rev")
+            self.add_trade(rev_trade, rev_spread_info)
 
         # if there was any trade
-        if self.cur_trade is not None:
-            # update & log individual balance
+        if new_trade or rev_trade:
+            # update each balance
             self.mm1.update_balance()
             self.mm2.update_balance()
 
-    def execute_trade(self, buy_order, sell_order):
-        self.cur_trade = Trade(TradeTag.NEW, [buy_order, sell_order], TradeMeta({}))
-        self.trade_manager.add_trade(self.cur_trade)
+    def execute_trade(self, spread_info: SpreadInfo, trade_type: str = "new" or "rev"):
+        if trade_type == "new":
+            buying_mkt = self.mm1
+            selling_mkt = self.mm2
+            buying_currency = self.mm1_currency
+            selling_currency = self.mm2_currency
+        elif trade_type == "rev":
+            buying_mkt = self.mm2
+            selling_mkt = self.mm1
+            buying_currency = self.mm2_currency
+            selling_currency = self.mm1_currency
+        else:
+            raise Exception("Invalid trade type!")
 
-    @staticmethod
-    def trade_logic(spread_info: SpreadInfo, buying_mkt, selling_mkt, target_currency, buying_currency,
-                    selling_currency, init_setting_dict: dict, trade_tag: str = "new" or "rev",
-                    is_init_setting_opt: bool = False):
         # check condition
-        threshold_cond = spread_info.tradable_spread >= init_setting_dict[trade_tag]["threshold"] >= 0
-        min_coin_cond = spread_info.tradable_qty >= init_setting_dict["min_trading_coin"]
+        threshold_cond = spread_info.tradable_spread >= self.init_setting_dict[trade_type]["threshold"] >= 0
+        min_coin_cond = spread_info.tradable_qty >= self.init_setting_dict["min_trading_coin"]
 
-        if threshold_cond and min_coin_cond:
-            # get fee
-            fee, should_fee = BasicAnalyzer.get_fee_consideration(buying_mkt.get_market_tag(), target_currency)
-            # apply fee if any
-            trading_amount = spread_info.tradable_qty + fee if should_fee else spread_info.tradable_qty
+        # quit if conditions don't meet
+        if not (threshold_cond and min_coin_cond):
+            return None
 
-            # balance check
-            has_enough_krw = RfabBacktester.has_enough_coin_checker(buying_mkt, "krw",
-                                                                    spread_info.buy_price *
-                                                                    init_setting_dict[trade_tag]["factor"])
-            has_enough_coin = RfabBacktester.has_enough_coin_checker(selling_mkt, target_currency,
-                                                                     trading_amount *
-                                                                     init_setting_dict[trade_tag]["factor"])
-            # if enough krw & coin balance
-            if has_enough_krw and has_enough_coin:
-                # make buy & sell order
-                buy_order = buying_mkt.order_buy(buying_currency, spread_info.buy_price, trading_amount)
-                sell_order = selling_mkt.order_sell(selling_currency, spread_info.sell_price, trading_amount)
-                # subtract considered fee if there was one
-                if should_fee:
-                    GlobalFeeAccumulator.sub_fee_consideration(buying_mkt.get_market_tag(), target_currency, fee)
-                # log executed trade info
-                if not is_init_setting_opt:
-                    TradeInfoLogger.execute_trade_log_info(trade_tag, spread_info, trading_amount)
-                return buy_order, sell_order
-            # if not enough balance
-            else:
-                if not is_init_setting_opt:
-                    # log failed trade (not enough mkt amount)
-                    TradeInfoLogger.not_enough_balance_log_info(trade_tag, spread_info)
-                return None, None
-        return None, None
+        # get fee
+        fee, should_fee = BasicAnalyzer.get_fee_consideration(buying_mkt.get_market_tag(), self.target_currency)
+        # apply fee if any
+        trading_amount = spread_info.tradable_qty + fee if should_fee else spread_info.tradable_qty
+
+        # balance check
+        krw_needed = spread_info.buy_price * self.init_setting_dict[trade_type]["factor"]
+        coin_needed = trading_amount * self.init_setting_dict[trade_type]["factor"]
+        has_enough_krw = RfabBacktester.has_enough_coin_checker(buying_mkt, "krw", krw_needed)
+        has_enough_coin = RfabBacktester.has_enough_coin_checker(selling_mkt, self.target_currency, coin_needed)
+
+        # if enough krw & coin balance
+        if not (has_enough_krw and has_enough_coin):
+            if not self.is_init_setting_opt:
+                TradeInfoLogger.not_enough_balance_log_info(trade_type, spread_info)
+            return None
+
+        # make buy & sell order
+        buy_order = buying_mkt.order_buy(buying_currency, spread_info.buy_price, trading_amount)
+        sell_order = selling_mkt.order_sell(selling_currency, spread_info.sell_price, trading_amount)
+        # subtract considered fee if there was one
+        if should_fee:
+            GlobalFeeAccumulator.sub_fee_consideration(buying_mkt.get_market_tag(), self.target_currency, fee)
+        return Trade(TradeTag.NEW, [buy_order, sell_order], TradeMeta({}))
 
     @staticmethod
     def has_enough_coin_checker(market, coin_type: str, needed_amount: float):
@@ -153,23 +130,18 @@ class RfabBacktester:
         else:
             return True
 
-    @staticmethod
-    def initialize_mongo(mm_dict: dict, target_currency: str):
-        method_name = {
-            Market.VIRTUAL_CO: "get_coinone_db",
-            Market.VIRTUAL_KB: "get_korbit_db",
-            Market.VIRTUAL_GP: "get_gopax_db"
-        }[mm_dict["mkt_tag"]]
-        return getattr(SharedMongoClient, method_name)()[target_currency + "_orderbook"]
-
 
 class TradeInfoLogger:
-    def __init__(self, trade_manager, target_currency, new_oppty_counter, rev_oppty_counter):
-
+    def __init__(self, trade_manager, target_currency):
         self.trade_manager = trade_manager
         self.target_currency = target_currency
-        self.new_oppty_counter = new_oppty_counter
-        self.rev_oppty_counter = rev_oppty_counter
+        self.new_oppty_counter = 0
+        self.rev_oppty_counter = 0
+
+    def acc_oppty_counter(self, trade_type: str):
+        attr_name = trade_type + "_oppty_counter"
+        oppty_count = getattr(self, attr_name)
+        setattr(self, attr_name, oppty_count + 1)
 
     def log_trade_info(self):
         trade_total = self.trade_manager.get_trade_count()
@@ -198,11 +170,12 @@ class TradeInfoLogger:
                          (coin_name, balance["available"], balance["trade_in_use"], balance["balance"]))
 
     @staticmethod
-    def execute_trade_log_info(trade_tag, spread_info, trading_amount):
+    def execute_trade_log_info(trade: Trade, spread_info: SpreadInfo):
+        trade_tag = trade.trade_tag.name.upper()
+        trade_amount = trade.orders[0].order_amount
         logging.warning("[EXECUTE] %s ->"
                         "Trading INFOS: Spread in unit = %.2f, Traded Spread = %.2f, Traded_QTY = %.5f"
-                        % (trade_tag.upper(), spread_info.spread_in_unit, spread_info.tradable_spread,
-                           trading_amount))
+                        % (trade_tag, spread_info.spread_in_unit, spread_info.tradable_spread, trade_amount))
 
     @staticmethod
     def not_enough_balance_log_info(trade_tag, spread_info):
--- run_risk_free_arb_bot_backtest.py
+++ run_risk_free_arb_bot_backtest.py
@@ -1,42 +1,49 @@
 from config.global_conf import Global
+from trader.market.market import Market
+from trader.base_arb_bot import BaseArbBot
 from config.shared_mongo_client import SharedMongoClient
-from trader.market.order import Market
 from backtester.risk_free_arb_backtest import RfabBacktester
+from trader.market_manager.virtual_market_manager import VirtualMarketManager
 
-Global.configure_default_root_logging(should_log_to_file=True)
-SharedMongoClient.initialize(should_use_localhost_db=False)
 
-start_time = Global.convert_local_datetime_to_epoch("2018.06.24 09:00:00", timezone="kr")
-end_time = Global.convert_local_datetime_to_epoch("2018.06.24 16:45:00", timezone="kr")
+def get_target_col(market_tag: Market, target_currency: str):
+    method_name = {
+        Market.VIRTUAL_CO: "get_coinone_db",
+        Market.VIRTUAL_KB: "get_korbit_db",
+        Market.VIRTUAL_GP: "get_gopax_db"
+    }[market_tag]
+    return getattr(SharedMongoClient, method_name)()[target_currency + "_orderbook"]
 
-target_currency = "bch"
 
-mm1_balance_dict = {
-    "mkt_tag": Market.VIRTUAL_CO,
-    "market_fee": 0.001,
-    "krw_balance": 5000000,
-    "coin_balance": 0.5
-}
+def main():
+    Global.configure_default_root_logging(should_log_to_file=True)
+    SharedMongoClient.initialize(should_use_localhost_db=False)
 
-mm2_balance_dict = {
-    "mkt_tag": Market.VIRTUAL_GP,
-    "market_fee": 0.00075,
-    "krw_balance": 500000,
-    "coin_balance": 5
-}
+    start_time = Global.convert_local_datetime_to_epoch("2018.06.24 16:45:00", timezone="kr")
+    end_time = Global.convert_local_datetime_to_epoch("2018.06.25 09:45:00", timezone="kr")
 
-initial_setting_dict = {
-    "max_trading_coin": 0.01,
-    "min_trading_coin": 0,
-    "new": {
-        "threshold": 50,
-        "factor": 1
-    },
-    "rev": {
-        "threshold": 50,
-        "factor": 1
+    initial_setting_dict = {
+        "max_trading_coin": 0.01,
+        "min_trading_coin": 0,
+        "new": {
+            "threshold": 10,
+            "factor": 1
+        },
+        "rev": {
+            "threshold": 10,
+            "factor": 1
+        }
     }
-}
 
-RfabBacktester(mm1_balance_dict, mm2_balance_dict, initial_setting_dict, start_time, end_time,
-               target_currency=target_currency, is_init_setting_opt=False).run()
+    target_currency = "bch"
+    mm1 = VirtualMarketManager(Market.VIRTUAL_CO, 0.001, 5000000, 0.5, target_currency)
+    mm2 = VirtualMarketManager(Market.VIRTUAL_GP, 0.00075, 500000, 5, target_currency)
+    mm1_col = get_target_col(Market.VIRTUAL_CO, target_currency)
+    mm2_col = get_target_col(Market.VIRTUAL_GP, target_currency)
+
+    mm1_data_cursor, mm2_data_cursor = BaseArbBot.get_data_from_db(mm1_col, mm2_col, start_time, end_time)
+    RfabBacktester(mm1, mm2, target_currency, initial_setting_dict, False).run(mm1_data_cursor, mm2_data_cursor)
+
+
+if __name__ == '__main__':
+    main()
