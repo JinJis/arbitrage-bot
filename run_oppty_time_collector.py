fe56546c7f46038b7159b37ce235a3da6e0d8289
dingzhen93 <chungjin93@gmail.com>
2018-07-01 02:39:17 +0900

    launcher for oppty_time_collector changed

--- run_oppty_time_collector.py
+++ run_oppty_time_collector.py
@@ -1,32 +1,32 @@
+import logging
 from trader.market.market import Market
 from trader.market_manager.virtual_market_manager import VirtualMarketManager
-from trader.risk_free_arb_bot import BaseArbBot
 from config.global_conf import Global
 from config.shared_mongo_client import SharedMongoClient
-from collector.oppty_time_collector import OpptyRequestTimeCollector
+from collector.oppty_time_collector import OpptyTimeCollector
 from optimizer.initial_setting_optimizer import InitialSettingOptimizer
 
 Global.configure_default_root_logging(should_log_to_file=False)
 SharedMongoClient.initialize(should_use_localhost_db=True)
 
-start_time = Global.convert_local_datetime_to_epoch("2018.06.30 09:00:00", timezone="kr")
-end_time = Global.convert_local_datetime_to_epoch("2018.06.30 13:00:00", timezone="kr")
+start_time = Global.convert_local_datetime_to_epoch("2018.06.29 12:00:00", timezone="kr")
+end_time = Global.convert_local_datetime_to_epoch("2018.06.29 15:00:00", timezone="kr")
 
 target_currency = "bch"
 mm1 = VirtualMarketManager(Market.VIRTUAL_CO, 0.001, 5000000, 0.5, target_currency)
 mm2 = VirtualMarketManager(Market.VIRTUAL_GP, 0.00075, 500000, 5, target_currency)
 mm1_col = SharedMongoClient.get_target_col(Market.VIRTUAL_CO, target_currency)
 mm2_col = SharedMongoClient.get_target_col(Market.VIRTUAL_GP, target_currency)
-mm1_data_cursor, mm2_data_cursor = BaseArbBot.get_data_from_db(mm1_col, mm2_col, start_time, end_time)
+mm1_data_cursor, mm2_data_cursor = SharedMongoClient.get_data_from_db(mm1_col, mm2_col, start_time, end_time)
 
-result_dict = OpptyRequestTimeCollector(mm1, mm2, target_currency).run(mm1_data_cursor, mm2_data_cursor)
+result_dict = OpptyTimeCollector(target_currency, mm1, mm2).run(mm1_data_cursor, mm2_data_cursor)
 
 print(result_dict)
 
 # get total duration time for each trade
-total_dur_dict = OpptyRequestTimeCollector.get_total_duration_time(result_dict)
+total_dur_dict = OpptyTimeCollector.get_total_duration_time(result_dict)
 for key in total_dur_dict.keys():
-    print("Total [%s] duration (hour): %.2f" % (key.upper(), (total_dur_dict[key] / 60 / 60)))
+    logging.info("Total [%s] duration (hour): %.2f" % (key.upper(), (total_dur_dict[key] / 60 / 60)))
 
 # Run ISO with start, end time under oppty
 for trade_type in result_dict.keys():
@@ -89,6 +89,6 @@ for trade_type in result_dict.keys():
             "start_time": time[0],
             "end_time": time[1]
         }
+        logging.critical("Now in: [%s] start_time: %d, end_time: %d" % (trade_type.upper(), time[0], time[1]))
         opt = InitialSettingOptimizer().run(settings, factor_settings)
-        print("RESULT ===> [%s] start_time: %d, end_time: %d" % (trade_type.upper(), time[0], time[1]))
-        print(opt)
+        logging.critical("Result: %s" % opt)
