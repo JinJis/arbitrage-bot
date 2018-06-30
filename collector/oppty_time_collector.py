b9e776b0794d4742cbd07a027697b89d1934c42d
dingzhen93 <chungjin93@gmail.com>
2018-07-01 02:40:06 +0900

    minor changes

--- collector/oppty_time_collector.py
+++ collector/oppty_time_collector.py
@@ -4,41 +4,37 @@ from analyzer.analyzer import BasicAnalyzer
 from trader.market_manager.virtual_market_manager import VirtualMarketManager
 
 
-class OpptyRequestTimeCollector:
+class OpptyTimeCollector:
     TARGET_STRATEGY = ATSAnalyzer.actual_tradable_spread_strategy
-    INTERVAL = 5
+    CONSECUTION_GRACE_TIME = 60
 
-    def __init__(self, mm1: VirtualMarketManager, mm2: VirtualMarketManager, target_currency: str):
+    def __init__(self, target_currency: str, mm1: VirtualMarketManager, mm2: VirtualMarketManager):
         self.target_currency = target_currency
         self.mm1 = mm1
         self.mm2 = mm2
-        self.raw_rq_time_dict = dict(new=[], rev=[])
 
     def run(self, mm1_data_cursor: Cursor, mm2_data_cursor: Cursor):
-
         # loop through history data
+        raw_rq_time_dict = dict(new=[], rev=[])
         for mm1_data, mm2_data in zip(mm1_data_cursor, mm2_data_cursor):
-            self.add_oppty_requesttime_to_list(mm1_data, mm2_data)
+            # get spread
+            new_unit_spread, rev_unit_spread = self.get_spread_info(mm1_data, mm2_data,
+                                                                    self.mm1.market_fee, self.mm2.market_fee)
+            # collect requestTime when NEW
+            if new_unit_spread > 0:
+                raw_rq_time_dict["new"].append(mm1_data["requestTime"])
+
+            # collect requestTime when NEW
+            if rev_unit_spread > 0:
+                raw_rq_time_dict["rev"].append(mm1_data["requestTime"])
 
         # sort raw rq time into duration
-        new_oppty_duration = OpptyRequestTimeCollector.sort_by_time_duration(self.raw_rq_time_dict["new"])
-        rev_oppty_duration = OpptyRequestTimeCollector.sort_by_time_duration(self.raw_rq_time_dict["rev"])
+        new_oppty_duration = OpptyTimeCollector.sort_by_time_duration(raw_rq_time_dict["new"])
+        rev_oppty_duration = OpptyTimeCollector.sort_by_time_duration(raw_rq_time_dict["rev"])
         return dict(new=new_oppty_duration, rev=rev_oppty_duration)
-
-    def add_oppty_requesttime_to_list(self, mm1_data: dict, mm2_data: dict):
-        new_unit_spread, rev_unit_spread = self.get_spread_info(mm1_data, mm2_data,
-                                                                self.mm1.market_fee, self.mm2.market_fee)
-        # collect requestTime when NEW
-        if new_unit_spread > 0:
-            self.raw_rq_time_dict["new"].append(mm1_data["requestTime"])
-
-        # collect requestTime when NEW
-        if rev_unit_spread > 0:
-            self.raw_rq_time_dict["rev"].append(mm1_data["requestTime"])
 
     @classmethod
     def get_spread_info(cls, mm1_orderbook: dict, mm2_orderbook: dict, mm1_market_fee: float, mm2_market_fee: float):
-
         # mm1 price
         mm1_minask_price, mm1_maxbid_price = BasicAnalyzer.get_price_of_minask_maxbid(mm1_orderbook)
         # mm2 price
@@ -58,29 +54,20 @@ class OpptyRequestTimeCollector:
 
     @classmethod
     def sort_by_time_duration(cls, rq_time_list: list):
-        was_in_oppty = False
-        temp_time_set = list()
         result = list()
-        for index, item in enumerate(rq_time_list[1:]):
-            now = rq_time_list[index + 1]
-            before = rq_time_list[index]
-            # when in oppty
-            if now - before == cls.INTERVAL:
-                if not was_in_oppty:
-                    was_in_oppty = True
-                    temp_time_set.append(before)
-            # when not in oppty
-            else:
-                if was_in_oppty:
-                    was_in_oppty = False
-                    temp_time_set.append(before)
-                    result.append([i for i in temp_time_set])
-                    temp_time_set.clear()
-        # when the last item is under oppty
-        if was_in_oppty:
-            temp_time_set.append(rq_time_list[-1])
-            result.append([i for i in temp_time_set])
-            temp_time_set.clear()
+        start = None
+        prev = None
+        for index, item in enumerate(rq_time_list):
+            if prev is None:
+                start = item
+                prev = item
+                continue
+            current = item
+            if (current - prev) > cls.CONSECUTION_GRACE_TIME:
+                end = prev
+                result.append([start, end])
+                start = current
+            prev = current
         return result
 
     @staticmethod
--- run_risk_free_arb_bot_backtest.py
+++ run_risk_free_arb_bot_backtest.py
@@ -1,6 +1,5 @@
 from config.global_conf import Global
 from trader.market.market import Market
-from trader.base_arb_bot import BaseArbBot
 from config.shared_mongo_client import SharedMongoClient
 from backtester.risk_free_arb_backtester import RfabBacktester
 from trader.market_manager.virtual_market_manager import VirtualMarketManager
@@ -10,8 +9,8 @@ def main():
     Global.configure_default_root_logging(should_log_to_file=True)
     SharedMongoClient.initialize(should_use_localhost_db=False)
 
-    start_time = Global.convert_local_datetime_to_epoch("2018.06.27 12:30:00", timezone="kr")
-    end_time = Global.convert_local_datetime_to_epoch("2018.06.27 12:50:00", timezone="kr")
+    start_time = Global.convert_local_datetime_to_epoch("2018.06.28 13:50:00", timezone="kr")
+    end_time = Global.convert_local_datetime_to_epoch("2018.06.29 14:50:00", timezone="kr")
 
     initial_setting_dict = {
         "max_trading_coin": 0.06624,
@@ -25,14 +24,13 @@ def main():
             "factor": 1
         }
     }
-
     target_currency = "bch"
     mm1 = VirtualMarketManager(Market.VIRTUAL_CO, 0.001, 5000000, 0.5, target_currency)
     mm2 = VirtualMarketManager(Market.VIRTUAL_GP, 0.00075, 500000, 5, target_currency)
     mm1_col = SharedMongoClient.get_target_col(Market.VIRTUAL_CO, target_currency)
     mm2_col = SharedMongoClient.get_target_col(Market.VIRTUAL_GP, target_currency)
 
-    mm1_data_cursor, mm2_data_cursor = BaseArbBot.get_data_from_db(mm1_col, mm2_col, start_time, end_time)
+    mm1_data_cursor, mm2_data_cursor = SharedMongoClient.get_data_from_db(mm1_col, mm2_col, start_time, end_time)
     RfabBacktester(mm1, mm2, "bch").run(mm1_data_cursor, mm2_data_cursor, initial_setting_dict, False)
 
 
