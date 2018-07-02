0bfbe13b82dfa73a06e7340b42b746d4d86d8a40
dingzhen93 <chungjin93@gmail.com>
2018-07-03 00:23:52 +0900

    IBO & ISO oppty time collector launcher done

--- run_oppty_time_collector.py
+++ run_oppty_time_collector.py
@@ -5,6 +5,7 @@ from config.global_conf import Global
 from config.shared_mongo_client import SharedMongoClient
 from collector.oppty_time_collector import OpptyTimeCollector
 from optimizer.initial_setting_optimizer import InitialSettingOptimizer
+from optimizer.initial_balance_optimizer import InitialBalanceOptimizer
 
 Global.configure_default_root_logging(should_log_to_file=False)
 SharedMongoClient.initialize(should_use_localhost_db=True)
@@ -13,8 +14,8 @@ start_time = Global.convert_local_datetime_to_epoch("2018.06.29 12:00:00", timez
 end_time = Global.convert_local_datetime_to_epoch("2018.06.29 15:00:00", timezone="kr")
 
 target_currency = "bch"
-mm1 = VirtualMarketManager(Market.VIRTUAL_CO, 0.001, 5000000, 0.5, target_currency)
-mm2 = VirtualMarketManager(Market.VIRTUAL_GP, 0.00075, 500000, 5, target_currency)
+mm1 = VirtualMarketManager(Market.VIRTUAL_CO, 0.001, 100000, 1, target_currency)
+mm2 = VirtualMarketManager(Market.VIRTUAL_GP, 0.00075, 100000, 1, target_currency)
 mm1_col = SharedMongoClient.get_target_col(Market.VIRTUAL_CO, target_currency)
 mm2_col = SharedMongoClient.get_target_col(Market.VIRTUAL_GP, target_currency)
 mm1_data_cursor, mm2_data_cursor = SharedMongoClient.get_data_from_db(mm1_col, mm2_col, start_time, end_time)
@@ -28,47 +29,10 @@ total_dur_dict = OpptyTimeCollector.get_total_duration_time(result_dict)
 for key in total_dur_dict.keys():
     logging.info("Total [%s] duration (hour): %.2f" % (key.upper(), (total_dur_dict[key] / 60 / 60)))
 
-# Run ISO with start, end time under oppty
+# loop through oppty times
 for trade_type in result_dict.keys():
     for time in result_dict[trade_type]:
         # Settings for ISO
-        factor_settings = {
-            "max_trading_coin": {
-                "start": 0,
-                "end": 0.1,
-                "step_limit": 0.0001
-            },
-            "min_trading_coin": {
-                "start": 0,
-                "end": 0,
-                "step_limit": 0
-            },
-            "new": {
-                "threshold": {
-                    "start": 0,
-                    "end": 1000,
-                    "step_limit": 1
-
-                },
-                "factor": {
-                    "start": 1,
-                    "end": 3,
-                    "step_limit": 0.01
-                }
-            },
-            "rev": {
-                "threshold": {
-                    "start": 0,
-                    "end": 1000,
-                    "step_limit": 1
-                },
-                "factor": {
-                    "start": 1,
-                    "end": 3,
-                    "step_limit": 0.01
-                }
-            }
-        }
         settings = {
             "target_currency": "bch",
             "mm1": {
@@ -89,6 +53,42 @@ for trade_type in result_dict.keys():
             "start_time": time[0],
             "end_time": time[1]
         }
+
+        factor_settings = {
+            "max_trading_coin": {"start": 0, "end": 0.05, "step_limit": 0.0001},
+            "min_trading_coin": {"start": 0, "end": 0, "step_limit": 0},
+            "new": {
+                "threshold": {"start": 0, "end": 1000, "step_limit": 1},
+                "factor": {"start": 1, "end": 3, "step_limit": 0.01}
+            },
+            "rev": {
+                "threshold": {"start": 0, "end": 1000, "step_limit": 1},
+                "factor": {"start": 1, "end": 3, "step_limit": 0.01}
+            }
+        }
+
+        bal_factor_settings = {
+            "mm1": {
+                "krw_balance": {"start": 0, "end": 10000000, "step_limit": 10000
+                                },
+                "coin_balance": {"start": 0, "end": 10, "step_limit": 0.1
+                                 }
+            },
+            "mm2": {
+                "krw_balance": {"start": 0, "end": 10000000, "step_limit": 10000
+                                },
+                "coin_balance": {"start": 0, "end": 10, "step_limit": 0.1
+                                 }
+            }
+        }
         logging.critical("Now in: [%s] start_time: %d, end_time: %d" % (trade_type.upper(), time[0], time[1]))
-        opt = InitialSettingOptimizer().run(settings, factor_settings)
-        logging.critical("Result: %s" % opt)
+
+        """"RUN ISO (Initial Setting Optimizer)"""
+        # iso_opt = InitialSettingOptimizer().run(settings, factor_settings)
+        # logging.critical("[ISO] Opted Result: %s" % iso_opt)
+
+        """"RUN IBO (Initial Balance Optimizer)"""
+        ibo_opt = InitialBalanceOptimizer.run(settings, bal_factor_settings)  # <-- Fix default Factor setting in Class
+        ibo_opt_yield = ibo_opt[0] / (ibo_opt[4]["mm1"]["krw_balance"] + ibo_opt[4]["mm2"]["krw_balance"]) * 100
+        logging.critical("[IBO] Opted Result: %s" % ibo_opt)
+        logging.critical("[IBO] Opted KRW Yield: %.4f" % ibo_opt_yield)
