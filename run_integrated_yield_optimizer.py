20591bb7a90c8e3378eb195aae6139079a3e982c
dingzhen93 <chungjin93@gmail.com>
2018-08-15 23:38:14 +0900

    for PAST IYO Nohup (5.01 ~ 5.31)

--- run_integrated_yield_optimizer.py
+++ run_integrated_yield_optimizer.py
@@ -1,15 +1,17 @@
 import logging
+import itertools as it
 from config.global_conf import Global
 from config.trade_setting_config import TradeSettingConfig
 from config.shared_mongo_client import SharedMongoClient
 from optimizer.arbitrage_combination_optimizer.integrated_yield_optimizer import IntegratedYieldOptimizer
 
 
-def main(coin_name: str, mm1: str, mm2: str):
+def main(coin_name: str, init_time: str, final_time: str):
     Global.configure_default_root_logging(should_log_to_file=False, log_level=logging.INFO)
-    SharedMongoClient.initialize(should_use_localhost_db=False)
+    SharedMongoClient.initialize(should_use_localhost_db=True)
+    db_client = SharedMongoClient.instance()
 
-    time_list = ["2018.08.13 00:00:00", "2018.08.14 00:00:00"]
+    time_list = make_time_list(init_time, final_time)
 
     prev_time = None
     for cur_time in time_list:
@@ -23,46 +25,64 @@ def main(coin_name: str, mm1: str, mm2: str):
         # draw iyo_config for bal & factor_setting
         iyo_config = Global.read_iyo_setting_config(coin_name)
 
-        # set settings, bal_fact_settings, factor_settings
-        settings = TradeSettingConfig.get_settings(mm1=mm1,
-                                                   mm2=mm2,
-                                                   target_currency=coin_name,
-                                                   start_time=start_time, end_time=end_time,
-                                                   division=iyo_config["division"],
-                                                   depth=iyo_config["depth"],
-                                                   consecution_time=iyo_config["consecution_time"],
-                                                   is_virtual_mm=True)
+        # FIXME: 빗썸 등등 거래소 생긴 날부터는 밑에 주석 쓰기
+        # rfab_combi_list = Global.get_rfab_combination_list(coin_name)
+        rfab_combi_list = list(it.combinations(["coinone", "gopax", "korbit"], 2))
+        for _combi in rfab_combi_list:
+            logging.critical("[%s-%s-%s] IYO conducting -> start_time: %s, end_time: %s" % (
+                coin_name.upper(), str(_combi[0]).upper(), str(_combi[1]).upper(), start_time, end_time))
 
-        bal_factor_settings = TradeSettingConfig.get_bal_fact_settings(iyo_config["krw_seq_end"])
+            # set settings, bal_fact_settings, factor_settings
+            settings = TradeSettingConfig.get_settings(mm1=_combi[0],
+                                                       mm2=_combi[1],
+                                                       target_currency=coin_name,
+                                                       start_time=start_time, end_time=end_time,
+                                                       division=iyo_config["division"],
+                                                       depth=iyo_config["depth"],
+                                                       consecution_time=iyo_config["consecution_time"],
+                                                       is_virtual_mm=True)
 
-        factor_settings = TradeSettingConfig.get_factor_settings(iyo_config["max_trade_coin_end"],
-                                                                 iyo_config["threshold_end"],
-                                                                 iyo_config["factor_end"],
-                                                                 iyo_config["appx_unit_coin_price"])
+            bal_factor_settings = TradeSettingConfig.get_bal_fact_settings(iyo_config["krw_seq_end"])
 
-        iyo_result = IntegratedYieldOptimizer.run(settings, bal_factor_settings, factor_settings)
-        """
-            <data in IYO_result structure>
-            1)  result = [combined_dict, combined_dict, combined_dict, ... ]
-            2)  combined_dict or cur_optimized = {
-                    "total_krw_invested: float,
-                    "krw_earned": float,
-                    "yield" : float,
-                    "new_traded": int,
-                    "rev_traded": int,
-                    "end_balance": dict,
-                    "settings": dict,
-                    "initial_setting": dict,
-                    "balance_setting": dict,
-                    "new_oppty_count": int,
-                    "rev_oppty_count": int
-                }
-        """
-        # stat analysis and append to db result
-        print(iyo_result)
-        logging.warning("Nohup done, now conducting next time set!!")
-        prev_time = cur_time
+            factor_settings = TradeSettingConfig.get_factor_settings(iyo_config["max_trade_coin_end"],
+                                                                     iyo_config["threshold_end"],
+                                                                     iyo_config["factor_end"],
+                                                                     iyo_config["appx_unit_coin_price"])
+
+            iyo_result = IntegratedYieldOptimizer.run(settings, bal_factor_settings, factor_settings)
+
+            # finally save to mongoDB
+            if len(iyo_result) > 0:
+                db_client["statistics"]["iyo"].insert_many(iyo_result)
+            else:
+                logging.critical("There was no oppty!! Skipping to next combination!")
+                continue
+
+            logging.warning("Nohup done, now conducting next time set!!")
+            prev_time = cur_time
+
+
+def make_time_list(init_time: str, final_time: str):
+    cur_time = init_time
+    time_list = [init_time]
+    while int(cur_time[8:10]) <= int(final_time[8:10]):
+        cur_time_day = cur_time[8:10]
+        if int(cur_time_day) < 9:
+            next_time_day = ("0%d" % (int(cur_time_day) + 1))
+        else:
+            next_time_day = str(int(cur_time_day) + 1)
+        cur_time = cur_time[:8] + next_time_day + cur_time[10:]
+        time_list.append(cur_time)
+
+    return time_list
 
 
 if __name__ == '__main__':
-    main(coin_name="bch", mm1="coinone", mm2="gopax")
+
+    s_time = '2018.05.01 00:00:00'
+    e_time = '2018.05.31 00:00:00'
+
+    Global.send_to_slack_channel("IYO Initiated!! start_time: %s, end_time: %s" % (s_time, e_time))
+    for target_currency in ["bch", "btc", "eth"]:
+        main(target_currency, s_time, e_time)
+    Global.send_to_slack_channel("IYO for past date set done for all COMBINATION!! ")
