1c5d2f407092908134738fc242f74852eaa238dc
dingzhen93 <chungjin93@gmail.com>
2018-08-17 03:29:53 +0900

    nohup test

--- collector/scheduler/otc_scheduler.py
+++ collector/scheduler/otc_scheduler.py
@@ -11,7 +11,7 @@ from collector.oppty_time_collector import OpptyTimeCollector
 class OTCScheduler(BaseScheduler):
     interval_time_sec = 10
     time_dur_to_anal = 24 * 60 * 60
-    publishing_time = "03:16:00"
+    publishing_time = "03:35:00"
 
     def __init__(self):
         Global.configure_default_root_logging(should_log_to_file=True, log_level=logging.CRITICAL)
