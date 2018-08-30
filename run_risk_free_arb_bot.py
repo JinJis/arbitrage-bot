171b9d9709222c7dfebd1eb645b9195380478a4a
dingzhen93 <chungjin93@gmail.com>
2018-08-31 00:56:42 +0900

    for nohup

--- run_risk_free_arb_bot.py
+++ run_risk_free_arb_bot.py
@@ -5,8 +5,8 @@ from trader.market_manager.coinone_market_manager import CoinoneMarketManager
 from config.shared_mongo_client import SharedMongoClient
 from trader.risk_free_arb_bot_v3 import RiskFreeArbBotV3
 
-Global.configure_default_root_logging(should_log_to_file=False, log_level=logging.INFO)
-SharedMongoClient.initialize(should_use_localhost_db=False)
+Global.configure_default_root_logging(should_log_to_file=True, log_level=logging.INFO)
+SharedMongoClient.initialize(should_use_localhost_db=True)
 streamer_db = SharedMongoClient.get_streamer_db()
 
 RiskFreeArbBotV3(
