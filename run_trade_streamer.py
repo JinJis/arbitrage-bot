a572c3eb389aeaae75ef705cd20599923d043522
dingzhen93 <chungjin93@gmail.com>
2018-09-04 18:26:31 +0900

    for nohup test

--- run_trade_streamer.py
+++ run_trade_streamer.py
@@ -2,12 +2,16 @@ import logging
 from config.global_conf import Global
 from config.shared_mongo_client import SharedMongoClient
 from trader.trade_manager.trade_streamer import TradeStreamer
-from trader.market_manager.market_manager import MarketManager
 from trader.market_manager.okcoin_market_manager import OkcoinMarketManager
 from trader.market_manager.bithumb_market_manager import BithumbMarketManager
 
 
-def main(mm1: MarketManager, mm2: MarketManager, target_currency: str):
+def main():
+
+    mm1 = BithumbMarketManager()
+    mm2 = OkcoinMarketManager()
+    target_currency = "xrp"
+
     Global.configure_default_root_logging(should_log_to_file=True, log_level=logging.WARNING)
     SharedMongoClient.initialize(should_use_localhost_db=True)
     # run TradeStreamer
@@ -15,4 +19,4 @@ def main(mm1: MarketManager, mm2: MarketManager, target_currency: str):
 
 
 if __name__ == '__main__':
-    main(BithumbMarketManager(), OkcoinMarketManager(), "xrp")
+    main()
