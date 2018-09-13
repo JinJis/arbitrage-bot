9877bbabd521db7bfafda0a09e71be27866fcfd8
dingzhen93 <chungjin93@gmail.com>
2018-09-13 09:13:25 +0900

    network connection

--- run_trade_streamer_v2.py
+++ run_trade_streamer_v2.py
@@ -8,11 +8,11 @@ from trader.market_manager.bithumb_market_manager import BithumbMarketManager
 
 
 def main(mm1: MarketManager, mm2: MarketManager, target_currency: str):
-    Global.configure_default_root_logging(should_log_to_file=True, log_level=logging.WARNING)
+    Global.configure_default_root_logging(should_log_to_file=False, log_level=logging.WARNING)
     SharedMongoClient.initialize(should_use_localhost_db=True)
     # run TradeStreamer
     TradeStreamerV2(mm1=mm1, mm2=mm2, target_currency=target_currency).run()
 
 
 if __name__ == '__main__':
-    main(BithumbMarketManager(), OkcoinMarketManager(), "xrp")
+    main(OkcoinMarketManager(), BithumbMarketManager(), "xrp")
