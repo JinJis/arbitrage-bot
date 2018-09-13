5de668f9e90907d593bb0050a331c5534994b304
dingzhen93 <chungjin93@gmail.com>
2018-09-13 11:00:49 +0900

    for test

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import ccxt
 
-ccxt.bithumb()
+bithumb = ccxt.bithumb()
+bithumb.apiKey = "6dcbd22b2c31aad9b16a3fa5ed61a468"
+bithumb.secret = "ec502fc57d861764032302b21d0b3a15"
 
+print(bithumb.fetch_balance())
