cde09704c3ae0ca9a480d4e724f3786c214692c7
dingzhen93 <chungjin93@gmail.com>
2018-09-18 01:04:29 +0900

    some refactoring done for RevLedgerXLSX

--- collector/rev_ledger_to_xlsx.py
+++ collector/rev_ledger_to_xlsx.py
@@ -8,7 +8,7 @@ from config.shared_mongo_client import SharedMongoClient
 from config.global_conf import Global
 
 
-class RevLedgerXLXS:
+class RevLedgerXLSX:
     DEFAULT_DIR = os.path.dirname(__file__) + "/rev_ledger_excel/"
     BASE_FILE_DIR = DEFAULT_DIR + "base_rev_ledger.xlsx"
     CRITERIA_COLUMN = {
@@ -34,13 +34,11 @@ class RevLedgerXLXS:
 
         try:
             self.file_dir = self.DEFAULT_DIR + '%s_%s_%s.xlsx' % (target_currency, mm1_name, mm2_name)
-            self.target_wb: Workbook \
-                = load_workbook(self.file_dir)
+            self.target_wb: Workbook = load_workbook(self.file_dir)
             self.target_ws: Worksheet = self.target_wb["ledger"]
         except FileNotFoundError:
-            logging.error("Filed Not Found!! Now creating New Rev Ledger xlxs!")
+            logging.error("Filed Not Found!! Now creating New Rev Ledger xlsx!")
             self.write_new_ledger()
-            logging.warning("New Rev Ledger created with designated combination")
 
     def run(self, mode_status: str):
         try:
@@ -66,31 +64,37 @@ class RevLedgerXLXS:
         # append mode_status
         self.target_ws[self.CRITERIA_COLUMN["mode_status"] + target_row].value = mongo_rev_ledger["mode_status"]
 
+        # if Initiation -> init balance
         if mongo_rev_ledger["mode_status"] == "initiation":
             for key1 in ["krw", "coin"]:
                 for key2 in ["mm1", "mm2", "total"]:
                     self.target_ws[self.CRITERIA_COLUMN[key1][key2] + target_row].value \
                         = mongo_rev_ledger["initial_bal"][key1][key2]
 
+        # if Settlement -> current balance
         if mongo_rev_ledger["mode_status"] == "settlement":
             for key1 in ["krw", "coin"]:
                 for key2 in ["mm1", "mm2", "total"]:
                     self.target_ws[self.CRITERIA_COLUMN[key1][key2] + target_row].value \
                         = mongo_rev_ledger["current_bal"][key1][key2]
 
+        # save to existing file
         self.target_wb.save(self.file_dir)
-        logging.critical("Excel Revenue Ledger saved successfully!!")
+        logging.warning("Excel Revenue Ledger saved successfully!!")
 
     def write_new_ledger(self):
-        self.target_wb: Workbook \
-            = load_workbook(self.BASE_FILE_DIR)
+        self.target_wb: Workbook = load_workbook(self.BASE_FILE_DIR)
         self.target_ws: Worksheet = self.target_wb["ledger"]
 
+        # after doing this, excel funxction will automatically change table names
         self.target_ws["C3"] = self.target_currency
         self.target_ws["D3"] = self.mm1_name
         self.target_ws["E3"] = self.mm2_name
 
+        # save to new combination named file
         self.target_wb.save(self.DEFAULT_DIR + '%s_%s_%s.xlsx' % (self.target_currency, self.mm1_name, self.mm2_name))
+
+        logging.warning("New Rev Ledger created with designated combination")
 
     def get_latest_mongo_rev_ledger(self, mode_status: str = ("initiation" or "settlement")):
         # sort rev_ledger of MongoDB by target combination
