3eb44e2872fb9252b44f04624d126334b7ce65bf
dingzhen93 <chungjin93@gmail.com>
2018-09-22 14:19:51 +0900

    for nohup

--- run_risk_free_arb_bot_v4.py
+++ run_risk_free_arb_bot_v4.py
@@ -6,7 +6,7 @@ from trader.market_manager.bithumb_market_manager import BithumbMarketManager
 from trader.market_manager.okcoin_market_manager import OkcoinMarketManager
 from trader.risk_free_arb_bot_v4 import RiskFreeArbBotV4
 
-Global.configure_default_root_logging(should_log_to_file=False, log_level=logging.WARNING)
-SharedMongoClient.initialize(should_use_localhost_db=False)
+Global.configure_default_root_logging(should_log_to_file=True, log_level=logging.WARNING)
+SharedMongoClient.initialize(should_use_localhost_db=True)
 
 RiskFreeArbBotV4(target_currency="xrp", mm1=BithumbMarketManager(), mm2=OkcoinMarketManager(), is_test=False).run()
