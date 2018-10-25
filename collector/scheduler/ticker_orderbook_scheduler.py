3164aeac8629237d5cdc1a11f1d8fc183d1e4bc9
dingzhen93 <chungjin93@gmail.com>
2018-10-25 21:21:05 +0900

    for scheduler

--- collector/scheduler/ticker_orderbook_scheduler.py
+++ collector/scheduler/ticker_orderbook_scheduler.py
@@ -8,9 +8,9 @@ from collector.scheduler.base_scheduler import BaseScheduler
 class TickerOrderbookScheduler(ApiScheduler):
     """
     - XRP(리플) --> 코인네스트 없음
-    - TRX(트론) --> 고팍스, 코빗, 코인원 없음
+    - TRX(트론) --> 고팍스, 코빗, 코인원, 없음
     - QTUM(퀀텀) --> 코빗 없음
-    - EOS -->
+    - EOS --> 코인네스트 없음
     """
 
     @BaseScheduler.interval_waiter(5)
@@ -19,7 +19,7 @@ class TickerOrderbookScheduler(ApiScheduler):
 
         Global.run_threaded(self.bt_collector.collect_orderbook, [request_time])
         Global.run_threaded(self.co_collector.collect_orderbook, [request_time])
-        # Global.run_threaded(self.kb_collector.collect_orderbook, [request_time])
+        Global.run_threaded(self.kb_collector.collect_orderbook, [request_time])
         Global.run_threaded(self.go_collector.collect_orderbook, [request_time])
         Global.run_threaded(self.oc_collector.collect_orderbook, [request_time])
         # Global.run_threaded(self.cn_collector.collect_orderbook, [request_time])
