723d6d538c853890be2d9a36e89f9709e3aa8316
jin.__.j <31362342+dingzhen93@users.noreply.github.com>
2018-12-18 20:25:13 +0900

    gitignore and api test update

--- test_api.py
+++ test_api.py
@@ -1,21 +1,22 @@
-# from api.bithumb_api import BithumbApi, BithumbCurrency
-from api.coinone_api import CoinoneApi, CoinoneCurrency
+from api.bithumb_api import BithumbApi, BithumbCurrency
+# from api.coinone_api import CoinoneApi, CoinoneCurrency
 # from api.korbit_api import KorbitApi, KorbitCurrency
 # from api.gopax_api import GopaxApi, GopaxCurrency
-# from api.okcoin_api import OkcoinApi, OkcoinCurrency
+from api.okcoin_api import OkcoinApi, OkcoinCurrency
 # from api.coinnest_api import CoinnestApi, CoinnestCurrency
 # from trader.market.order import Order, OrderType
 # from trader.market.market import Market
 
 
-# bt_api = BithumbApi.instance(is_public_access_only=False)
-co_api = CoinoneApi.instance(is_public_access_only=False)
+bt_api = BithumbApi.instance(is_public_access_only=False)
+# co_api = CoinoneApi.instance(is_public_access_only=False)
 # kb_api = KorbitApi.instance()
 # gp_api = GopaxApi.instance(is_public_access_only=False)
-# bt_api = BithumbApi.instance(is_public_access_only=False)
+oc_api = OkcoinApi.instance(is_public_access_only=False)
 # cn_api = CoinnestApi.instance()
 
 # order = Order(Market.COINONE, CoinoneCurrency.TRON, OrderType.LIMIT_BUY, "70862443", 344300, 0.01)
-result = co_api.get_balance()
+result = bt_api.get_balance()["krw"]["available"]
+result2 = oc_api.get_balance()["krw"]["available"]
 # result = bt_api.order_limit_buy(BithumbCurrency.ETH, 343000, 0.01)
-print(result)
+print(result + result2)
