3ff9706272b3cf2ea432d6924d2c724e1d6fb87f
jin.__.j <31362342+dingzhen93@users.noreply.github.com>
2018-12-25 18:31:46 +0900

    minor update

--- test_api.py
+++ test_api.py
@@ -3,6 +3,7 @@ from api.bithumb_api import BithumbApi, BithumbCurrency
 # from api.korbit_api import KorbitApi, KorbitCurrency
 # from api.gopax_api import GopaxApi, GopaxCurrency
 from api.okcoin_api import OkcoinApi, OkcoinCurrency
+
 # from api.coinnest_api import CoinnestApi, CoinnestCurrency
 # from trader.market.order import Order, OrderType
 # from trader.market.market import Market
@@ -18,5 +19,9 @@ oc_api = OkcoinApi.instance(is_public_access_only=False)
 # order = Order(Market.COINONE, CoinoneCurrency.TRON, OrderType.LIMIT_BUY, "70862443", 344300, 0.01)
 result = bt_api.get_balance()["krw"]["available"]
 result2 = oc_api.get_balance()["krw"]["available"]
+
+result3 = bt_api.get_balance()["xrp"]["available"]
+result4 = oc_api.get_balance()["xrp"]["available"]
 # result = bt_api.order_limit_buy(BithumbCurrency.ETH, 343000, 0.01)
 print(result + result2)
+print(float(result3) + float(result4))
